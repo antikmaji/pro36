f3b66a1cadd0bdbaea58cdc4224d07c1171f9ce1
antikmaji <74954769+antikmaji@users.noreply.github.com>
2021-03-12 23:29:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,71 @@
+var dog,sadDog,happyDog,database;
+
+
+function preload(){
+  sadDog=loadImage("Images/Dog.png");
+  happyDog=loadImage("Images/happy dog.png");
+}
+
+function setup() {
+  createCanvas(1000,400);
+
+  database = firebase.database();
+  console.log(database);
+ 
+  foodObj=new Food();
+  
+  dog=createSprite(800,200,150,150);
+  dog.addImage(sadDog);
+  dog.scale=0.15;
+
+  feed=createButton("Feed the dog");
+  feed.position(700,95);
+  feed.mousePressed(feedDog);
+
+  addfood=createButton("Add Food")
+  addfood.position(800,95);
+  addfood.mousePressed(addFood);
+
+}
+
+function draw() {
+  background(46,139,87);
+  drawSprites();
+}
+
+//function to read food Stock
+
+
+//function to update food stock and last fed time
+
+function feedDog()
+{
+
+  dog.addImage(happyDog);
+
+  if(foodObj.getFoodStock()<=0){
+
+foodObj.updateFoodStock(foodObj.getFoodStock()*0)
+
+  }else{
+
+    foodObj.updateFoodStock(foodObj.getFoodStock()-1)
+
+  } 
+
+}
+
+
+//function to add food in stock
+
+function addFood(){
+
+foodObj++;
+
+database.ref('/').update({
+Food:foodS
+})
+
+}
+
+
